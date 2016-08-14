bf8b397875042a77e8b6a268b4b52e25d87e7f05
Doelia <kyxalia@gmail.com>
2016-08-14 16:53:46 +0200

    Affichage des pokémons à venir

--- app/game.component.js
+++ app/game.component.js
@@ -18,6 +18,10 @@ var GameComponent = (function () {
         this.addToDone = new core_1.EventEmitter();
         this.addToFailed = new core_1.EventEmitter();
         this.id = 1;
+        this.curentIRandom = 0;
+        this.list_random = [];
+        this.list_nexts = [];
+        this.maxPokemon = 151;
         this.next();
     }
     GameComponent.prototype.onGood = function () {
@@ -30,17 +34,49 @@ var GameComponent = (function () {
     };
     GameComponent.prototype.next = function () {
         var _this = this;
+        if (this.curentIRandom >= this.list_random.length) {
+            this.randomList();
+            this.curentIRandom = 0;
+        }
         this.id = null;
-        setTimeout(function () { return _this.id = _this.random(); }, 50);
+        var new_id = this.list_random[this.curentIRandom++];
+        setTimeout(function () { return _this.id = new_id; }, 50);
+        this.list_nexts = [];
+        for (var i = 0; i < 10; i++) {
+            this.list_nexts.push(this.list_random[this.curentIRandom + i]);
+        }
+    };
+    GameComponent.prototype.shuffle = function (array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    };
+    GameComponent.prototype.randomList = function () {
+        var list = [];
+        for (var i = 0; i <= this.maxPokemon; i++) {
+            list.push(i);
+        }
+        list = this.shuffle(list);
+        this.list_random = list;
+        console.log('randomList', this.list_random);
     };
     GameComponent.prototype.random = function () {
-        var max = 151;
+        var max = this.maxPokemon;
         return Math.floor((Math.random() * max) + 1);
     };
     GameComponent = __decorate([
         core_1.Component({
             selector: 'game',
-            template: "\n  <div class=\"find-it\">\n      <find-it *ngIf=\"id\" [id]=\"id\" (onGood)=\"onGood()\"></find-it>\n      <br>\n      <button class=\"btn-primary btn\" (click)=\"skip()\">Passer au suivant</button>\n   </div>\n   <div>\n       <h3>Pok\u00E9mons trouv\u00E9s</h3>\n       <dones class=\"success\" [onGood]=\"addToDone\"></dones>\n   </div>\n   <div>\n       <h3>Pok\u00E9mons non trouv\u00E9s</h3>\n       <dones class=\"failed\" [onGood]=\"addToFailed\"></dones>\n   </div>\n  ",
+            template: "\n  <div class=\"find-it\">\n      <find-it *ngIf=\"id\" [id]=\"id\" (onGood)=\"onGood()\"></find-it>\n      <br>\n      <button class=\"btn-primary btn\" (click)=\"skip()\">Passer au suivant</button>\n   </div>\n   <div>\n        <div *ngFor=\"let id of list_nexts\">\n            <div class=\"pogo pokemon-{{id}} pogo\"></div>\n        </div>\n   </div>\n   <div>\n       <h3>Pok\u00E9mons trouv\u00E9s</h3>\n       <dones class=\"success\" [onGood]=\"addToDone\"></dones>\n   </div>\n   <div>\n       <h3>Pok\u00E9mons non trouv\u00E9s</h3>\n       <dones class=\"failed\" [onGood]=\"addToFailed\"></dones>\n   </div>\n  ",
             providers: [],
             directives: [find_it_component_1.FindItComponent, dones_component_1.DonesComponent]
         }), 
--- app/game.component.ts
+++ app/game.component.ts
@@ -12,6 +12,11 @@ import { DonesComponent } from './dones.component';
       <button class="btn-primary btn" (click)="skip()">Passer au suivant</button>
    </div>
    <div>
+        <div *ngFor="let id of list_nexts">
+            <div class="pogo pokemon-{{id}} pogo"></div>
+        </div>
+   </div>
+   <div>
        <h3>Pokémons trouvés</h3>
        <dones class="success" [onGood]="addToDone"></dones>
    </div>
@@ -29,6 +34,12 @@ export class GameComponent {
     addToFailed = new EventEmitter();
     id: number = 1;
 
+    curentIRandom = 0;
+    list_random = [];
+    list_nexts = [];
+
+    maxPokemon = 151;
+
     constructor(private dico: PokemonDicoService) {
         this.next();
     }
@@ -44,12 +55,51 @@ export class GameComponent {
     }
 
     next() {
+        if (this.curentIRandom >= this.list_random.length) {
+            this.randomList();
+            this.curentIRandom = 0;
+        }
+
         this.id = null;
-        setTimeout(() => this.id = this.random(), 50);
+        let new_id = this.list_random[this.curentIRandom++];
+        setTimeout(() => this.id = new_id, 50);
+
+        this.list_nexts = [];
+        for (let i = 0; i < 10; i++) {
+            this.list_nexts.push(this.list_random[this.curentIRandom+i]);
+        }
+    }
+
+    private shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
+    randomList() {
+        let list = [];
+        for (let i=0; i <= this.maxPokemon; i++) {
+            list.push(i);
+        }
+        list = this.shuffle(list);
+        this.list_random = list;
+        console.log('randomList', this.list_random);
     }
 
     random() {
-        let max = 151;
+        let max = this.maxPokemon;
         return Math.floor((Math.random() * max) + 1);
     }
 }
